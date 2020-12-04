b5db6536726a4dc36d86f1350f773b70685f591d
Bryan Teh Yue Hung <kyle.19932@hotmail.com>
2020-12-04 20:43:15 +0800

    typo

--- week04/script.js
+++ week04/script.js
@@ -7,7 +7,7 @@ let elUsername = document.getElementById("namaPengguna");
 elUsername.addEventListener("keyup", transformUpperCase);
 
 function checkPasswordLength(){
-    let password = docutment.getElementById("kataLaluan").value;
+    let password = document.getElementById("kataLaluan").value;
     if (password.length < 5 ){
 alert ("Password must be more than 5 charater")
     }else {
