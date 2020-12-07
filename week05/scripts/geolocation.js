61eb9e93a4396c984d75f5a25cdd79541e9d6afa
Bryan Teh Yue Hung <kyle.19932@hotmail.com>
2020-12-07 20:02:53 +0800

    geolocation

---
+++ week05/scripts/geolocation.js
@@ -0,0 +1,15 @@
+let elLocate = document.getElementById ("locate")
+elLocate.addEventListener("click",function(){
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(){
+            let ellat = document.getElementById("lat");
+            let ellong = document.getElementById("long");
+
+            ellat.innerHTML = "Latitude : " + position.coords.latitude;
+            ellong.innerHTML = "Longtitude : " + position.coords.longitude;
+
+        });
+    }else{
+        alert("Geolocation is not supported by this browser.")
+    }
+});
