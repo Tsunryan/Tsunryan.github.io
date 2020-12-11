fa192767e37ae02c2a5d05800da657f5f83d79a4
Bryan Teh Yue Hung <kyle.19932@hotmail.com>
2020-12-11 20:50:47 +0800

    s

--- week06/scripts/ajax.js
+++ week06/scripts/ajax.js
@@ -16,7 +16,7 @@ function getRandomUser(){
     }
 }
 
-    xhttp.open('GET','https://randomuser.me/api/',true);
+    xhttps.open('GET','https://randomuser.me/api/',true);
     xhttps.send();
     }
 
