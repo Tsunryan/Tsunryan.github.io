b5eea5634416fbb380da178287a73e5e5ca260f7
Bryan Teh Yue Hung <kyle.19932@hotmail.com>
2020-12-11 20:28:05 +0800

    js

---
+++ week06/scripts/ajax.js
@@ -0,0 +1,20 @@
+function getRandomUser(){
+    let xhttps = new XMLHttpRequest() //create request object
+
+    xhttps.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200)
+            let data = JSON.parse(this.response);
+            let elFirstName = document.getElementById('FirstName');
+            let elLastName = document.getElementById('LastName');
+            let elJsonResult = document.getElementById('jsonResult');
+            let elUserImage = document.getElementById('UserImage');    
+            let elPhone = document.getElementById('phone');
+            let elAddress = document.getElementById('address');
+            let elEmail = document.getElementById('email');
+            
+            elJsonResult.innerHTML = this.response;
+    }
+
+    xhttp.open('GET','https://randomuser.me/api/', true);
+    xhttps.send();
+    };
