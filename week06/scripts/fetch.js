27f5ea70c9775ecace05f5dec6aae6431cc253c2
Bryan Teh Yue Hung <kyle.19932@hotmail.com>
2020-12-15 21:42:53 +0800

    2142

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -50,7 +50,7 @@ function BookNow(guestName,guestEmail,guestPax){
         // Do something with object
         //console.log(json.booking);
         let bookMsg = document.getElementById('bookMsg');
-        bookMsg.innerHTML = json.booking.name + "added!"
+        bookMsg.innerHTML = json.booking.name + " added!"
         GetBookings();
     });
 }
