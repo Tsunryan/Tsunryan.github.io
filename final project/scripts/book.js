7294003e382ec598eef69ace9488eb7b4746051f
Bryan Teh Yue Hung <kyle.19932@hotmail.com>
2020-12-27 10:45:06 +0800

    27/12 1045

---
+++ final project/scripts/book.js
@@ -0,0 +1,34 @@
+function BookNow(userName, userEmail, userPax, userRemarks) {
+    let url = 'https://api.sheety.co/f69038c6ee1e7950b8ca2f5803dcd15a/bookingApp/bookings';
+    let body = {
+        booking: {
+            name: userName,
+            email: userEmail,
+            pax: userPax,
+            remark: userRemarks
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => response.json())
+        .then(json => {
+            alert(json.booking.name + " added!");
+        });
+}
+
+window.addEventListener("load", function () {
+    document.getElementById("bookNow").addEventListener("click", function () {
+        let userName = document.getElementById("userName").value;
+        let userEmail = document.getElementById("userEmail").value;
+        let userPax = document.getElementById("userPax").value;
+        let userRemarks = document.getElementById("userRemarks").value;
+
+        BookNow(userName, userEmail, userPax, userRemarks);
+
+    });
+});
